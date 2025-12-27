0c1528ad239f1fd917cb842fab26addc78b5f42d
jamesmully40 <mullyjames40@gmail.com>
2025-12-27 16:40:26 +0300

    Add API proxy to vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,5 +8,12 @@ export default defineConfig({
     hmr: {
       overlay: false,
     },
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 })
